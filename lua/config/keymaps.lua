471109adb6e143542114572412132430a57228e4
eng1neeer_93 <raitermark@proton.me>
2024-08-24 11:23:29 +0300

    config

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,12 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+--
+
+
+vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
+vim.keymap.set('i', 'jj', '<Esc>')
+vim.keymap.set('n', '<Tab>', ':BufferLineCycleNext<CR>')
+vim.keymap.set('n', '<s-Tab>', ':BufferLineCyclePrev<CR>')
+vim.keymap.set('t', 'jk', [[<C-\><C-n>]])
+vim.keymap.set('n', '<leader>o', ':Neotree float git_status<CR>')
